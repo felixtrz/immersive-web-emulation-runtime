01ba936509ee5f9c4afc1765d6cf793e00a17f4d
Felix Z <felix@elixr.games>
2025-05-21 19:23:40 -0700

    Fix DOMPointReadOnly polyfill

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: 'ts-jest/presets/default-esm',
-	extensionsToTreatAsEsm: ['.ts'],
-	moduleNameMapper: {
-		'^(\\.{1,2}/.*)\\.js$': '$1',
-	},
+        preset: 'ts-jest/presets/default-esm',
+        extensionsToTreatAsEsm: ['.ts'],
+        setupFiles: ['<rootDir>/tests/polyfill.ts'],
+        moduleNameMapper: {
+                '^(\\.{1,2}/.*)\\.js$': '$1',
+        },
 	transform: {
 		'^.+\\.ts$': ['ts-jest', { useESM: true }],
 	},
